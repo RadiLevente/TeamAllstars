23defac55d171425134972bfd7a66b686b5fdb26
L.Radi <radi.levente@gmail.com>
2023-01-18 23:43:45 +0100

    almost done, animations, sound added

--- gamelab_project.js
+++ gamelab_project.js
@@ -2,10 +2,11 @@ World.frameRate = 60;
 
 var bg = createSprite(200, 200);
 bg.setAnimation("bg");
+bg.scale = 0.7;
 
 // var logo = createSprite(40,40);
 // logo.setAnimation("logo");
-// logo.scale = 0.7;
+// logo.scale = 0.2;
 
 var snake = createSprite(50, 350);
 snake.setAnimation("right");
@@ -32,22 +33,22 @@ logo.scale = 0.5;
 function randomizeValue() {
   //** esélyek kb. 40% - 0, 40% - 1, 20% - mentorBoost
   rnd = randomNumber(1, 5);
-  if (rnd <= 2) {
-    value = zeroes;
-  }
   if (rnd <= 4) {
     value = ones;
   }
-
+  if (rnd <= 2) {
+    value = zeroes;
+  }
   if (rnd == 5) {
     value = mentorBoost;
   }
 }
+console.log(rnd + " " + value);
 
 function draw() {
   drawSprites();
-
-  textSize(20);
+  textFont("Trebuchet MS");
+  textSize(30);
   if (pa > 80) {
     fill("green");
   }
@@ -57,7 +58,7 @@ function draw() {
   if (pa <= 50) {
     fill("red");
   }
-  text("PA: " + pa + " %", 70, 45);
+  text("PA: " + pa + " %", 30, 30);
 
   if (keyDown("right")) {
     snake.setSpeedAndDirection(speed, 0);
@@ -79,8 +80,43 @@ function draw() {
     snake.setAnimation("down");
   }
 
+  var a = logo.x;
+  var b = logo.y;
+
   if (snake.isTouching(logo)) {
     randomizeValue();
+
+    if (value == zeroes) {
+      var dino0 = createSprite(a, b);
+      dino0.setAnimation("0");
+      playSound(
+        "sound://category_achievements/lighthearted_bonus_objective_1.mp3"
+      );
+      setTimeout(function () {
+        dino0.destroy();
+      }, 500);
+    }
+
+    if (value == ones) {
+      var dino1 = createSprite(a, b);
+      dino1.setAnimation("1");
+      playSound(
+        "sound://category_achievements/lighthearted_bonus_objective_4.mp3"
+      );
+      setTimeout(function () {
+        dino1.destroy();
+      }, 500);
+    }
+
+    if (value == mentorBoost) {
+      var dino10 = createSprite(a, b);
+      dino10.setAnimation("10");
+      playSound("sound://category_achievements/vibrant_power_booster.mp3");
+      setTimeout(function () {
+        dino10.destroy();
+      }, 500);
+    }
+
     pa = pa + value;
     logo.x = randomNumber(30, 370);
     logo.y = randomNumber(30, 370);
@@ -101,13 +137,14 @@ function draw() {
   }
 
   createEdgeSprites();
+
   if (snake.isTouching(edges)) {
     snake.setSpeedAndDirection(0, 0);
     logo.visible = false;
+    snake.visible = false;
     var gameOver = createSprite(200, 200);
     gameOver.setAnimation("gameOver");
-    playSound("sound://category_explosion/8bit_explosion.mp3");
-    stopSound();
+    playSound("sound://category_digital/win.mp3");
   }
 
   if (pa >= 100) {
@@ -116,6 +153,6 @@ function draw() {
     snake.visible = false;
     textSize(50);
     fill("red");
-    text("Gratulálok, \n  full-stack dev \n lettél! :D", 50, 200);
+    text("Gratulálok,\nfull-stack dev\nlettél! :D", 50, 200);
   }
 }
